163b52c861fb27646ac6f453bcb2866a3282761c
Neha <nehaboora49@gmail.com>
2025-07-12 20:49:43 +0530

    completed about page

---
+++ src/utils/helper.js
@@ -0,0 +1,14 @@
+import { Circle } from "./icon"
+
+export const Who_We_Are_Data = [
+    {
+        title: 'OUR MISSION',
+        icon:Circle,
+        description:'Lorem ipsum dolor sit amet, consectetur adiping elit, sed do eiusmod temp incit ut labore dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp incididunt ut labore dol magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod temp incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipng elit, sed do eiusmod temp incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        title: 'OUR VISION',
+        icon:Circle,
+        description: 'Lorem ipsum dolor sit amet, consectetur adiping elit, sed do eiusmod temp incit ut labore dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp incididunt ut labore dol magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod temp incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam Lorem ipsum dolor sit amet, consectetur adipng elit, sed do eiusmod temp incididunt ut labore et dolore magna aliqua.',
+    },
+]
---
+++ src/utils/icon.jsx
@@ -0,0 +1,22 @@
+export const Circle = ({className}) => (
+    <svg
+    className={className}
+        width="51" height="50" viewBox="0 0 51 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <ellipse cx="25.083" cy="24.5756" rx="24.5756" ry="24.9981" transform="rotate(-90 25.083 24.5756)" fill="white" />
+        <g mask="url(#mask0_417_370)">
+            <rect width="29.7453" height="1.98302" rx="0.991509" transform="matrix(-0.713107 -0.701055 0.713107 -0.701055 19.3623 51.033)" fill="#FF9534" />
+            <rect width="29.7453" height="1.98302" rx="0.991509" transform="matrix(0.713107 0.701055 -0.713107 0.701055 30.5654 -1.90173)" fill="#FF9534" />
+            <rect width="39.6603" height="1.98302" rx="0.991509" transform="matrix(-0.713107 -0.701055 0.713107 -0.701055 25.7266 51.728)" fill="#FF9534" />
+            <rect width="39.6603" height="1.98302" rx="0.991509" transform="matrix(0.713107 0.701055 -0.713107 0.701055 24.2246 -2.57532)" fill="#FF9534" />
+            <rect width="46.6009" height="1.98302" rx="0.991509" transform="matrix(-0.713107 -0.701055 0.713107 -0.701055 31.3828 51.728)" fill="#FF9534" />
+            <rect width="46.6009" height="1.98302" rx="0.991509" transform="matrix(0.713107 0.701055 -0.713107 0.701055 18.5869 -2.55115)" fill="#FF9534" />
+            <rect width="53.5415" height="1.98302" rx="0.991509" transform="matrix(-0.713107 -0.701055 0.713107 -0.701055 37.04 51.728)" fill="#FF9534" />
+            <rect width="53.5415" height="1.98302" rx="0.991509" transform="matrix(0.713107 0.701055 -0.713107 0.701055 12.9502 -2.52698)" fill="#FF9534" />
+            <rect width="53.5415" height="1.98302" rx="0.991509" transform="matrix(-0.713107 -0.701055 0.713107 -0.701055 39.8682 48.9476)" fill="#FF9534" />
+            <rect width="53.5415" height="1.98302" rx="0.991509" transform="matrix(0.713107 0.701055 -0.713107 0.701055 10.1436 0.27478)" fill="#FF9534" />
+            <rect width="53.5415" height="1.98302" rx="0.991509" transform="matrix(-0.713107 -0.701055 0.713107 -0.701055 41.9893 45.4719)" fill="#FF9534" />
+            <rect width="53.5415" height="1.98302" rx="0.991509" transform="matrix(0.713107 0.701055 -0.713107 0.701055 8.0498 3.76636)" fill="#FF9534" />
+        </g>
+    </svg>
+
+)

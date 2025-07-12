b44740b68709787e3fbc96ea3c341edd10aed7a0
aman <gfc4150@gmail.com>
2025-07-12 12:22:27 +0530

    routs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import ContactUs from './pages/ContactUs'
+import Gallery from './pages/Gallery'
+import Management from './pages/Management'
+import News from './pages/News'
+import AboutUs from './pages/AboutUs'
 
 function App() {
 
   return (
     <>
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contect-us" element={<ContactUs />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/management" element={<Management />} />
+        <Route path="/news-&-events" element={<News />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
     </>
   )
 }
